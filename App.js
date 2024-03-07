8f95fc11ece46a1a611189b5b66dc18c86ddef51
ranevikram12 <ranevikram12@gmail.com>
2024-03-07 15:01:59 +0530

    second

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { useEffect, useState } from "react";
+
+import Home from "./Home";
 
 export default function App() {
+  
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
+
+
+    <Home />
     </View>
   );
 }
@@ -13,8 +23,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  donutChart: {
+    paddingTop: 50,
+    flex: 50,
+  },
+  barChart: {
+    flex: 50,
   },
 });
